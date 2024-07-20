385c7c480216a549be54f9653a0f8e783a20ac27
shukri1236 <shukri1236>
2024-07-20 19:50:42 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    let paragraph = document.getElementById("text")
+    paragraph.textContent = "This is really cool!"
+});
